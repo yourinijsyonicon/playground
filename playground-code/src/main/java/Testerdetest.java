c077c62d09a2446d5e306ba913b296695125591c
Youri Nijs <youri.nijs@yonicon.be>
2021-11-05 15:16:13 +0100

    our fantastic mob session

--- playground-code/src/main/java/Testerdetest.java
+++ playground-code/src/main/java/Testerdetest.java
@@ -1,2 +1,27 @@
+import java.math.BigInteger;
+
 public class Testerdetest {
+    private final String rareVariabele;
+    private Long blablabla;
+    private String blablkabla;
+
+    public Testerdetest(String rareVariabele, Long blablabla, String blablkabla) {
+        this.rareVariabele = rareVariabele;
+        this.blablabla = blablabla;
+        this.blablkabla = blablkabla;
+    }
+
+    public String getRareVariabele() {
+        return rareVariabele;
+    }
+
+    public Long getBlablabla() {
+        return blablabla;
+    }
+
+    public String getBlablkabla() {
+        return blablkabla;
+    }
+
+
 }
